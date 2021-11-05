9e81b81222bab6c1a8228450e3cc128513b91a44
bruno <bruno@colaco.com.br>
2021-11-04 22:27:13 -0300

    Group by search

---
+++ backend/src/main/java/com/colaco/dsvendas/dto/SaleSuccessDTO.java
@@ -0,0 +1,26 @@
+package com.colaco.dsvendas.dto;
+
+import com.colaco.dsvendas.entities.Seller;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SaleSuccessDTO implements Serializable {
+
+    private String sellerName;
+    private Long visited;
+    private Long deals;
+
+    public SaleSuccessDTO(Seller seller, Long visited, Long deals) {
+        sellerName = seller.getName();
+        this.visited = visited;
+        this.deals = deals;
+    }
+}
--- backend/src/main/java/com/colaco/dsvendas/services/SaleService.java
+++ backend/src/main/java/com/colaco/dsvendas/services/SaleService.java
@@ -1,6 +1,8 @@
 package com.colaco.dsvendas.services;
 
 import com.colaco.dsvendas.dto.SaleDTO;
+import com.colaco.dsvendas.dto.SaleSuccessDTO;
+import com.colaco.dsvendas.dto.SaleSumDTO;
 import com.colaco.dsvendas.entities.Sale;
 import com.colaco.dsvendas.repositories.SaleRepository;
 import com.colaco.dsvendas.repositories.SellerRepository;
@@ -9,6 +11,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 
 @Service
@@ -25,5 +29,15 @@ public class SaleService {
         sellerRepository.findAll();
         Page<Sale> result = saleRepository.findAll(pageable);
         return result.map(x -> new SaleDTO(x));
+    }
+
+    @Transactional(readOnly = true)
+    public List<SaleSumDTO> amountGroupedBySeller(){
+        return saleRepository.amountGroupedBySeller();
+    }
+
+    @Transactional(readOnly = true)
+    public List<SaleSuccessDTO> successGroupedBySeller(){
+        return saleRepository.sucessGroupedBySeller();
     }
 }
